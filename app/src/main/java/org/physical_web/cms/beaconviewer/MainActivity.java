ca48373fe85feb1705e6f064f738db167ec93ddc
Kayali <me@kayali.io>
2017-08-17 14:42:28 +0300

    Base application

---
+++ app/src/main/java/org/physical_web/cms/beaconviewer/MainActivity.java
@@ -0,0 +1,13 @@
+package org.physical_web.cms.beaconviewer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
